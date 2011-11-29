0121bb093ee72226e9dc559f108ec8337ff5d5fb
Yoon Lee <yoonlees@uiuc.edu>
2011-11-29 15:22:03 -0600

    Initila Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'b0d3677d9b456e16c24d31ac2e5075008e8ec86b9a0bec2bafacae016083954eaf6528cb07a7dd839eb3bde798288a16b1b928ac9cc104d3a2ff0de6eac51785'
